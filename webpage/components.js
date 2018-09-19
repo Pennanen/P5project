7b59462abecab95e5983645e5464a15bc5d86b3a
Pennanen Matias TTV16SP <matiaspennanen@kamk.fi>
2018-09-19 14:41:29 +0300

    Created planetarium in JS

---
+++ webpage/components.js
@@ -0,0 +1,149 @@
+var click = 0;
+var buttonList = [];
+var ballList = [];
+
+function createButtons(maxButtons)
+{
+	for(var i = 1; i<=maxButtons;i++)
+	{
+	buttonI = new Button(40,38*i,170,32,'Nappi '+i);
+	buttonList.push(buttonI);
+	}
+	return buttonList;
+}
+function createBalls(maxBalls)
+{
+	var _c = color(255,255,255);
+	for(var i = 1; i<=maxBalls;i++)
+	{
+	ballI = new Ball(windowWidth/2-100+100*i,windowHeight/2,60,_c);
+	ballList.push(ballI);
+	}
+	return ballList;
+}
+
+class Slider{
+	constructor(_x,_y,_w,_h,_c,_tV,_tH){
+		this.w = _w;
+		this.h = _h;
+		this.c = _c;
+		
+		this.targetHidden = _tH;
+		this.targetVisible = _tV;
+		
+		this.targetX = 0;
+		this.x = _x;
+		this.startX = _x;
+		
+		this.targetY = 0;
+		this.y = _y;
+		this.startY = _y;
+	}
+	
+	update(){
+		
+	if (mouseX < this.startX+abs(this.w+10) & mouseX > this.startX-(abs(this.w-10)))
+		{
+		this.targetX = this.targetVisible;
+		}
+		else
+		{
+		this.targetX = this.targetHidden;
+		}
+		
+		this.x += (this.targetX - this.x)*0.1;
+		this.draw();
+	}
+	draw(){
+		fill(this.c);
+		noStroke();
+		rect(this.x,this.y,this.w,this.h);
+	}
+}
+
+class Ball{
+	constructor(_x,_y,_r,_c,_label)
+	{
+	this.x = _x;
+	this.y = _y;
+	this.r = _r;
+	this.c = _c;
+	this.label = _label;
+	}
+	update(pos)
+	{
+	this.x+=pos;
+	}
+	
+	draw()
+	{
+		if (this.x < windowWidth+this.r+100 && this.x > -this.r-100)
+		{
+		fill(this.c);
+		ellipse(this.x,this.y,this.r,this.r,this.label);
+		fill(color(255,255,255));
+		textAlign(LEFT);
+		text(this.label, this.x+this.r/2+100, this.y);
+		}
+	}
+}
+
+class Button
+{
+	constructor(_x,_y,_w,_h,_label){
+		this.x1 = _x;
+		this.y1 = _y;
+		this.label = _label;
+		this.w = _w;
+		this.h = _h;
+		this.active = 1;
+		this.x2 = this.x1+this.w;
+		this.y2 = this.y1+this.h;
+		this.cBase = color(130,130,180);
+		this.cSelect = color(165,140,255);
+		this.c = this.cBase;
+		this.clicked = false;
+	}
+	
+	update(_targetX)
+	{
+	this.x1 = _targetX;
+	
+	if (click == 1)
+		{
+		if (mouseX > this.x1 && mouseX < this.x2 && mouseY > this.y1 && mouseY < this.y2 && !this.clicked)
+			{
+				buttonList.active = -1;
+				this.active *= -1;
+				this.clicked = true;
+			}
+			if (this.active == -1) {this.c = this.cSelect;}
+			else {this.c = this.cBase;};
+		}
+		else
+		{
+			this.clicked = false;
+			//this.active = 1; //disable other buttons? Works almost
+		}
+		
+		
+	}
+	
+	draw(){
+		fill(this.c);
+		noStroke();
+		rect(this.x1,this.y1,this.w,this.h);
+		textSize(26);
+		fill(color(255,255,255));
+		textAlign(CENTER,CENTER);
+		text(this.label, this.x1+this.w/2, this.y1+this.h/2);
+	}
+}
+
+function mousePressed() {
+	buttons.active = -1;
+if (click==0)click = 1;
+}
+function mouseReleased() {
+click = 0;
+}
--- webpage/sketch.js
+++ webpage/sketch.js
@@ -1,45 +1,76 @@
 let sLeft,sRight;
-var mic;
-function preload() {
+var buttons = [];
+var Planets = [];
+var targetpos = 0;
+var sunSize = 6000;
+var Au = 3000;
+function setup() 
+{
+	mainCanvas = createCanvas(windowWidth,windowHeight-100);
+	cSlider = color(50,50,150);
+	sLeft = new Slider(0,0,200,windowHeight,cSlider,0,-170);
+
+	mainCanvas.position(0,100);
+	h1 = createElement('h1','ASD');
+	h1.position(windowWidth-100,10);
+  //sRight = new Slider(windowWidth,0,-300,windowHeight,cSlider,windowWidth-10,windowWidth+260);
+	buttons = createButtons(6);
 	
-}
-function setup() {
-  createCanvas(windowWidth,windowHeight);
-  cSlider = color(50,50,150);
-  sLeft = new Slider(
-				0,
-				0,
-				300,
-				windowHeight,
-				cSlider,
-				0,
-				-270);
-				
-  sRight = new Slider(
-				windowWidth,
-				0,
-				-300,
-				windowHeight,
-				cSlider,
-				windowWidth-10,
-				windowWidth+260);
-				
-		mic = new p5.AudioIn()
-		mic.start();
-		
+	balls = createBalls(12);
+	
+	sun = new Ball(-2500,windowHeight/2,sunSize,color(255, 199, 0));
+	
+	mercury = new Ball(windowWidth/2+Au*0.39,windowHeight/2,sunSize/277,color(60,60,60),"Mercury");
+	venus = new Ball(windowWidth/2+Au*0.72,windowHeight/2,sunSize/113,color(80,80,80),"Venus");
+	earth = new Ball(windowWidth/2+Au*1.00,windowHeight/2,sunSize/108,color(0, 212, 255),"Earth");
+	mars = new Ball(windowWidth/2+Au*1.52,windowHeight/2,sunSize/208,color(255, 99, 43),"Mars");
+	jupiter = new Ball(windowWidth/2+Au*5.2,windowHeight/2,sunSize/9.7,color(252, 172, 98),"Jupiter");
+	saturn = new Ball(windowWidth/2+Au*9.54,windowHeight/2,sunSize/11.4,color(252, 203, 97),"Saturn");
+	uranus = new Ball(windowWidth/2+Au*19.2,windowHeight/2,sunSize/26.8,color(196, 255, 255),"Uranus");
+	neptune = new Ball(windowWidth/2+Au*39.4,windowHeight/2,sunSize/27.7,color(32, 85, 247),"Neptune");
+	
+	Planets.push(sun);
+	Planets.push(mercury);
+	Planets.push(venus);
+	Planets.push(earth);
+	Planets.push(mars);
+	Planets.push(jupiter);
+	Planets.push(uranus);
+	Planets.push(neptune);
+	
+	
 }
 
 function draw() {
 	
 	background(10,10,10);
-	sLeft.update();
-	sRight.update();
-	micLevel = mic.getLevel();
 	
-	ellipse(windowWidth/2,windowHeight/2,100+micLevel*1000,100+micLevel*1000);
+	
+	for(var i = 0; i<Planets.length;i++){
+	Planets[i].update(targetpos);
+	Planets[i].draw();
+	}
+	
+	sLeft.update();
+	//sRight.update();
 
+	for(var i = 0; i<buttons.length;i++){
+	buttons[i].update(sLeft.x+15);
+	buttons[i].draw();
+	}
+
+	if (targetpos >= 2) {targetpos-=2}
+	else if (targetpos <= -2) {targetpos+=2}
+	else {targetpos = 0};
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  h1.position(windowWidth-100,10);
+  mainCanvas.position(0,100);
+}
+
+function mouseWheel(event)
+{
+	targetpos=event.delta*0.5;
 }
